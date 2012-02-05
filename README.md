c492fc05fe273f31c6e1cd44f51061ee9f74039c
Peter Petersson <petersson.peter@gmail.com>
2012-02-05 17:46:48 +0100

    The starting point.

---
+++ README.md
@@ -0,0 +1,7 @@
+FoBo - Lift Template
+--------------------
+
+This is a stipped down Lift template / FoBo demo for use with/of the FoBo Lift module. 
+See the [FoBo - Lift Front-End Toolkit Module](https://github.com/karma4u101/FoBo).
+
+Live demo of this lift template with fobo module demo is available [here](http://www.media4u101.se/fobo-lift-template-demo/). 
---
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,73 @@
+package bootstrap.liftweb
+
+import _root_.net.liftweb._
+import http.ResourceServer._
+import actor._
+import util._
+import Helpers._
+import common._
+import http._
+import sitemap._
+import Loc._
+import java.util.Locale
+import java.sql.DriverManager
+import _root_.net.liftweb.util.{ Props }
+import _root_.net.liftweb.http.provider.HTTPRequest
+import _root_.net.liftweb.http.auth.{ HttpBasicAuthentication, AuthRole, userRoles }
+
+//import nu.m4u.snippet._
+
+object localeOverride extends SessionVar[Box[Locale]](Empty)
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot extends Loggable {
+  def boot {
+
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    //Include the front-end toolkit module
+    net.liftmodules.FoBo.FoBo.init()
+ 
+    // Build SiteMap
+    val entries = List(
+      Menu.i("Home") / "index",
+      Menu(Loc("Static", Link(List("static"), true, "/static/index"), "Static Content")))      
+      
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    LiftRules.setSiteMap(SiteMap(entries: _*))
+
+    //Show the spinny image when an Ajax call starts
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+
+    // Make the spinny image go away when it ends
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    // Use jQuery 1.4
+    LiftRules.jsArtifacts = net.liftweb.http.js.jquery.JQuery14Artifacts
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    // Use HTML5 for rendering
+    LiftRules.htmlProperties.default.set((r: Req) =>
+      new Html5Properties(r.userAgent))
+
+    //notice fade out (start after x, fade out duration y)
+    LiftRules.noticesAutoFadeOut.default.set((notices: NoticeType.Value) => {
+      notices match {
+        case NoticeType.Notice => Full((8 seconds, 4 seconds))
+        case _                 => Empty
+      }
+    })
+
+  }
+
+}
+

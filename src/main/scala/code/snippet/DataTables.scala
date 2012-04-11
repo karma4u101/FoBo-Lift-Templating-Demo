57291ab62ff3c1e35e62a65e4d109d31aa9668e9
Peter Petersson <petersson.peter@gmail.com>
2012-04-11 09:04:37 +0200

    Updating the view with demo of the datatables snippet invocation.

--- src/main/scala/code/snippet/DataTables.scala
+++ src/main/scala/code/snippet/DataTables.scala
@@ -10,7 +10,6 @@ import code.lib.{DataTablesH=>ctrl}
 class DataTables extends StatefulSnippet with Loggable {
 
   lazy val ctrl = new ctrl()
-  //val ccList:List[City]  = City.getAllList()
   
    def dispatch = {
      case "semiDynamicDT" => semiDynamicDT
@@ -19,7 +18,6 @@ class DataTables extends StatefulSnippet with Loggable {
   def semiDynamicDT = {
        var on = S.attr("on") openOr "on: NOT DEFINED!?"
        var tableId = S.attr("tableId") openOr "tableId: NOT DEFINED!?"
-       //var options = S.attr("options") openOr ""
     " *" #> ctrl.semiDynamicJS(on,tableId)
   }
   
